6f956fd88f416ca6aa7c6fb573009ef7801625c5
Marcel Schmitt <marcel.mj.schmitt@web.de>
2020-07-31 21:39:40 +0200

    extend deno functionality, docker-compose

--- deno/index.ts
+++ deno/index.ts
@@ -1,28 +1,74 @@
-import { serve } from 'https://deno.land/std/http/server.ts'
 import { Gauge, Registry } from "https://deno.land/x/ts_prometheus/mod.ts";
+import { Application, Router } from "https://deno.land/x/oak/mod.ts";
+import { soxa } from 'https://deno.land/x/soxa/mod.ts'
+import "https://deno.land/x/dotenv/load.ts";
+import "https://deno.land/x/newdash/sleep.ts";
 
-/*
+const URL = Deno.env.get('URL');
+CheckEnvironmentVariable('URL', URL);
 
-HTTP SERVER
+const CLIENTID = Deno.env.get('CLIENTID');
+CheckEnvironmentVariable('CLIENTID', CLIENTID);
 
-*/ 
+const CLIENTPASSWORD = Deno.env.get('CLIENTPASSWORD');
+CheckEnvironmentVariable('CLIENTPASSWORD', CLIENTPASSWORD);
 
-const httpServer = serve({ port: 8000 })
-console.log(`🦕 Deno server running at http://localhost:8000/ 🦕`)
-
-for await (const req of httpServer) {
-  req.respond({ body: 'Hello from your first Deno server' })
+function CheckEnvironmentVariable(parameterName: any, parameterValue: any) {
+    if (!parameterValue) {
+        throw new Error(`${parameterName} is null or empty!`);
+      }
+    
+      console.log(`${parameterName}: ${parameterValue}`);
 }
 
-/*
 
-METRICS
+const gauge = Gauge.with({
+    name: 'ENDPOINT_AVAILABILITY',
+    help: 'Shows availability of endpoint.',
+    labels: ['mode'],
+});
 
-*/
+const app = new Application();
 
-const counter = Gauge.with({
-    name: "http_requests_total",
-    help: "The total HTTP requests",
-    labels: ["path", "method", "status"],
-  });
-  
+const router = new Router();
+router
+    .get("/hello", (ctx) => {
+        ctx.response.body = "Hello world!";
+    })
+    .get("/hello/:name", (ctx) => {
+        const { params } = ctx;
+        ctx.response.body = `Hello, ${params.name}!`;
+    })
+    .get("/metrics", (ctx) => {
+        ctx.response.headers.set("Content-Type", "");
+        ctx.response.body = Registry.default.metrics();
+    });
+
+app.use(async (ctx, next) => {
+    await next();
+});
+
+app.use(router.routes());
+
+//while (1) {
+    gauge.set(1);
+    try {
+        let response = await soxa.post(`${URL}`, {}, {
+            auth: {
+                username: `${CLIENTID}`,
+                password: `${CLIENTPASSWORD}`
+            }
+
+        });
+
+        console.log(response);
+        console.log(response.body);
+    } catch {
+        console.log("something went wrong");
+        gauge.set(0);
+    }
+
+    //sleep(10000);
+//}
+
+await app.listen({ port: 8080 });
